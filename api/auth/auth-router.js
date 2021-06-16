931a7b38ae01d28a7aff0430a27b58df5a800420
Johnny Murillo <jossters@gmail.com>
2021-06-15 23:33:59 -0700

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
-const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
+const { checkUsernameExists, validateRoleName } = require("./auth-middleware");
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const Users = require("../users/users-model");
+const bcrypt = require("bcryptjs");
+const tokenBuilder = require("./token-builder");
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
     response:
     status 201
     {
@@ -14,29 +16,41 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
+  const hash = bcrypt.hashSync(req.body.password, 8);
+  req.body.password = hash;
+  Users.add(req.body)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch(next);
 });
-
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
     response:
     status 200
     {
       "message": "sue is back!",
       "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
     }
-
     The token must expire in one day, and must provide the following information
     in its payload:
-
     {
       "subject"  : 1       // the user_id of the authenticated user
       "username" : "bob"   // the username of the authenticated user
       "role_name": "admin" // the role of the authenticated user
     }
    */
+  if (bcrypt.compareSync(req.body.password, req.user.password)) {
+    const token = tokenBuilder(req.user);
+    res.status(200).json({
+      message: `${req.body.username} is back!`,
+      token,
+    });
+  } else {
+    next({ status: 401, message: "invalid credentials" });
+  }
 });
 
 module.exports = router;
